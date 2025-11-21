de09c0ca105467ad7980aba4cfaf042663d64302
neoaia <151616379+neoaia@users.noreply.github.com>
2025-11-22 05:45:23 +0800

    notification  component start

--- app/(tabs)/(home)/notification.tsx
+++ app/(tabs)/(home)/notification.tsx
@@ -1,12 +1,18 @@
-import { View, Text } from "react-native";
+import { NotificationItem } from '@/components/notifications/notificationItem';
 import { auth } from '@/firebase';
 import { router } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function NotificationScreen() {
     return (
         <View className='flex justify-center items-center h-screen'>
             <Text>Notification Screen</Text>
             <Text>Email: {auth.currentUser?.email}</Text>
+            <NotificationItem type="water" />
+            <NotificationItem type="light" />
+            <NotificationItem type="harvest" />
+            <NotificationItem type="sensor" />
+            <NotificationItem type="environment" />
             <Text onPress={() => router.back()} className='underline text-red-800'> Go Back </Text>
         </View>
     )
